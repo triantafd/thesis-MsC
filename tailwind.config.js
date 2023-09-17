a17501807a2b878658f41ac9ff349348570c8a83
dimitris <triantafd@iti.gr>
2023-09-17 14:38:01 +0300

    add some boilerplate code for footer and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryCyan: 'hsl(180, 70%, 50%)',
+        lightCyan: 'hsl(180, 70%, 75%)',
+        softViolet: 'hsl(257, 15%, 60%)',
+        deepViolet: 'hsl(260, 15%, 16%)',
+
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+      },
+    },
   },
   plugins: [],
 }
