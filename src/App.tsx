ac8a1e7730bc6b3cb8389ecd6b955ee5fce18ed2
dimitris <triantafd@iti.gr>
2023-09-17 14:47:08 +0300

    add darkMode provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './layouts/Footer';
 import Header from './layouts/Header';
+import { useDarkMode } from './context/darkModeContext';
 
 function App() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+  
   return (
     <div className="App">
       <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <button onClick={toggleDarkMode}>
+            Toggle {darkMode ? 'Light' : 'Dark'} Mode
+          </button>
+          <div className={`bg-white dark:bg-gray-900`}>
+            This is your content. It'll be white in light mode and gray-900 in
+            dark mode.
+          </div>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
