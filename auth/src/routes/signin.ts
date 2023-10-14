326fb4cf23fc2ba08c1f670b82d9868fbd04d650
triantafd <dimitrios.triantaf@gmail.com>
2023-10-14 22:30:23 +0300

    minor fix

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,46 +1,38 @@
-/** @format */
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from 'thesis-common';
 
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import jwt from "jsonwebtoken";
-import { validateRequest, BadRequestError } from "thesis-common";
-
-import { Password } from "../services/password";
-import { User } from "../models/user";
+import { Password } from '../services/password';
+import { User } from '../models/user';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signin",
+  '/api/users/signin',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
       .trim()
       .notEmpty()
-      .withMessage("You must supply a password"),
+      .withMessage('You must supply a password'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    console.log("mpika mesa");
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      throw new BadRequestError("Invalid credentials");
+      throw new BadRequestError('Invalid credentials');
     }
-    console.log("Invalid credentials");
 
     const passwordsMatch = await Password.compare(
       existingUser.password,
       password
     );
-
-    console.log("Invalid passwordsMatch");
     if (!passwordsMatch) {
-      throw new BadRequestError("Invalid Credentials");
+      throw new BadRequestError('Invalid Credentials');
     }
-
-    console.log("JWT KEY", process.env.JWT_KEY);
 
     // Generate JWT
     const userJwt = jwt.sign(
@@ -55,7 +47,7 @@ router.post(
     req.session = {
       jwt: userJwt,
     };
-    console.log("existingUser KEY", req, existingUser);
+
     res.status(200).send(existingUser);
   }
 );
